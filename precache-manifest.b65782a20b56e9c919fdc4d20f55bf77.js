fcc0c584f4941e7e94a5a68651665dc0203fc7fb
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-08-22 15:11:16 +0300

    Updates

---
+++ precache-manifest.b65782a20b56e9c919fdc4d20f55bf77.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43b80e4257a304a927a9f4f02bed29d8",
+    "url": "/spa/index.html"
+  },
+  {
+    "revision": "ba7bcda086e1197fa772",
+    "url": "/spa/static/css/main.22880795.chunk.css"
+  },
+  {
+    "revision": "933f355775289d361a45",
+    "url": "/spa/static/js/2.872bbb17.chunk.js"
+  },
+  {
+    "revision": "ba7bcda086e1197fa772",
+    "url": "/spa/static/js/main.d33aa793.chunk.js"
+  },
+  {
+    "revision": "5401de05117730d07c95",
+    "url": "/spa/static/js/runtime~main.c7185bb9.js"
+  },
+  {
+    "revision": "b0aa499a3d7ab09c2800890096191881",
+    "url": "/spa/static/media/five.b0aa499a.jpg"
+  },
+  {
+    "revision": "99f57b5063d592df9afa56e0b80dde32",
+    "url": "/spa/static/media/four.99f57b50.jpg"
+  },
+  {
+    "revision": "c9f3a419ac2ca88acebb78dcb1c1131a",
+    "url": "/spa/static/media/header.c9f3a419.jpg"
+  },
+  {
+    "revision": "42f7ca285bb2a48d6f04950d99874d19",
+    "url": "/spa/static/media/one.42f7ca28.jpeg"
+  },
+  {
+    "revision": "1f14dc570a5d148b2c7f75c05aa83b98",
+    "url": "/spa/static/media/six.1f14dc57.jpg"
+  },
+  {
+    "revision": "a8ec8bd2109090bbb37e8cddd2d340ba",
+    "url": "/spa/static/media/three.a8ec8bd2.jpg"
+  },
+  {
+    "revision": "54b1edc117ce2c0dc10210da2bf049fd",
+    "url": "/spa/static/media/two.54b1edc1.jpg"
+  }
+]);
